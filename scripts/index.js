7da4bd4aa78c385eb108d7884800b87932fb27f6
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2025-06-04 18:12:41 +1000

    Add files via upload
    added the javascript in single_product.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,93 +1,29 @@
-const products = [
-  {
-    id: 1,
-    image: "images/image 4.jpeg",
-    name: "Rabbit Crochet",
-    rating: 5,
-    priceCents: 3999,
-  },
-  {
-    id: 2,
-    image: "images/image 4.jpeg",
-    name: "Elephant Crochet",
-    rating: 4,
-    priceCents: 1999,
-  },
-  {
-    id: 3,
-    image: "images/image 4.jpeg",
-    name: "Cat Crochet",
-    rating: 5,
-    priceCents: 2099,
-  },
-  {
-    id: 4,
-    image: "images/image 4.jpeg",
-    name: "Monkey Crochet",
-    rating: 4,
-    priceCents: 1099,
-  },
-  {
-    id: 5,
-    image: "images/image 4.jpeg",
-    name: "Teddy Crochet",
-    rating: 5,
-    priceCents: 2100,
-  },
-  {
-    id: 6,
-    image: "images/image 4.jpeg",
-    name: "Tiger Crochet",
-    rating: 4,
-    priceCents: 1999,
-  },
-  {
-    id: 7,
-    image: "images/image 4.jpeg",
-    name: "Giant Teddy Crochet",
-    rating: 5,
-    priceCents: 2099,
-  },
-  {
-    id: 8,
-    image: "images/image 4.jpeg",
-    name: "Little Rabbit Crochet",
-    rating: 4,
-    priceCents: 1099,
-  },
-];
-
-let productsHTML = '';
+let productsHTML = "";
 
 products.forEach((product) => {
-  productsHTML +=  ` <div class="crochet mt-3 text-center col-lg-3 col-md-4 col-sm-12">
+  productsHTML += ` <div class="crochet mt-3 text-center col-lg-3 col-md-4 col-sm-12">
           <img
             class="image-fluid rounded mb-3"
             src="${product.image}"
             alt="${product.name}"
           />
-          <div>${'⭐'.repeat(product.rating)}</div>
+          <div>${"⭐".repeat(product.rating)}</div>
           <p class="crochet-info">${product.name}</p>
           <p class="crochet-info">$${(product.priceCents / 100).toFixed(2)}</p>
-          <button class="shop-btn">Shop now</button>
+        
+         <button class="shop-btn" onclick="viewProduct('${product.id}')">Shop now</button>
         </div>`;
 });
 
-console.log(productsHTML);
+// console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 
 
-// document.getElementById("see-more-btn").addEventListener("click", function () {
-//   const moreImages = document.getElementById("more-images");
-
-//   moreImages.classList.toggle("d-none");
-
-//   this.textContent = moreImages.classList.contains("d-none")
-//     ? "See more"
-//     : "See less";
-// });
+function viewProduct(productId){
+  window.location.href=`single_product.html?id=${productId}`;
+}
 
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbxGmVgsymx36_iZwKeCBRZp83obY5CTioN0M3v4MuTUZE-c_qFU4IZMMepikXjWRJZBiQ/exec";
