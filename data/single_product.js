ead0855b436cfbeff5d1616ab5803fac009353f3
Halima <152661929+Halima-Ablat@users.noreply.github.com>
2025-06-04 18:09:43 +1000

    Add files via upload
    added cart.js and single_product.js

---
+++ data/single_product.js
@@ -0,0 +1,55 @@
+// Get product ID from URL
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("id");
+
+// Find the specific product
+const product = products.find((p) => p.id == productId);
+
+const singleCrochet = document.querySelector(".single-product");
+
+if (product) {
+  singleCrochet.innerHTML = `
+    <div class="col-lg-5 col-md-6 col-sm-12">
+      <img id="large-image" class="image-fluid product-img" src="${
+        product.image
+      }" alt="crochet" />
+   
+    </div>
+    <div class="col-lg-6 col-md-12 col-sm-12 crochet-detail px-5">
+      <h2>${product.name}</h2>
+      <p>$${(product.priceCents / 100).toFixed(2)}</p>
+      <label>Quantity</label><br />
+      <input class="quantity" type="number" value="1" />
+      <button class="add-btn mt-3" data-product-id="${
+        product.id
+      }">Add to cart</button>
+      <h4 class="my-5">Product details</h4>
+    
+    </div>
+  `;
+} else {
+  singleCrochet.innerHTML = `<h2>Product Not Found</h2>`;
+}
+
+document.querySelectorAll(".add-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  });
+});
